0b791a16f33b152d435645beee63f8dbe65de6f7
ParkDajeong <sobeast980@gmail.com>
2023-06-27 09:59:30 +0900

    Initial commit

---
+++ js/banner.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+  $('.visual').slick({
+    autoplay: true,
+    autoplaySpeed: 5000,
+    infinite: true,
+    dots: true,
+    dotsClass: 'banner-dots flex-box',
+    customPaging: (slider, i) => {
+      return `<button type="button" class="banner-dot flex-box font-amiri">
+        ${i + 1}<span class="blind">번 슬라이드로 이동</span>
+      </button>`;
+    },
+    arrows: false,
+    fade: true,
+    cssEase: 'linear',
+  });
+});
